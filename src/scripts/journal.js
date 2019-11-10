be168cbf025e04613bb42c548965663bd6f81555
Corri Golden <corri.golden@gmail.com>
2019-11-09 20:32:14 -0600

    working on rendering to DOM

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -9,9 +9,9 @@ const journalEntry = {
 
 console.log(journalEntry.dateOfEntry)
 
-let journalEntries = []
-journalEntries.push(journalEntry)
-console.log(journalEntries)
+// let journalEntries = []
+// journalEntries.push(journalEntry)
+// console.log(journalEntries)
 
 let secondEntry = {
     dateOfEntry: "11/02/2019",
@@ -27,5 +27,50 @@ let thirdEntry = {
     moodOfTheDay: "Happy",
 }
 
-journalEntries.push(secondEntry, thirdEntry)
-console.log("journalEntries")
+// journalEntries.push(secondEntry, thirdEntry)
+// console.log("journalEntries")
+
+let journalEntries = [
+    {
+        dateOfEntry: "11/01/2019",
+        conceptsCovered: "Objects",
+        journalEntry: "1",
+        moodOfTheDay: "Happy",
+    },
+
+    {
+        dateOfEntry: "11/02/2019",
+        conceptsCovered: "Arrays",
+        journalEntry: "2",
+        moodOfTheDay: "Sad",
+    },
+
+    {
+        dateOfEntry: "11/03/2019",
+        conceptsCovered: "Objects",
+        journalEntry: "3",
+        moodOfTheDay: "Happy",
+    }
+]
+
+
+const makeJournalEntryComponent = (journalEntry) => {
+    return `
+    <h1>Array Methods</h1>
+    <h2>We learned about array methods, but only for Each made sense.</h2>
+    <h2>07/24/2019</h2>
+    `
+}
+console.log(makeJournalEntryComponent)
+
+const renderDom = {
+    renderJournalEntries(entries) {
+        let HtmlForAllEntries = ""
+        journalEntries.forEach(entry => {
+            const entryHTML = makeJournalEntryComponent(entry)
+            HtmlForAllEntries += entryHTML
+        })
+        const logArticle = document.querySelector(".entryLog")
+        logArticle.innerHTML = HtmlForAllEntries
+    }
+}
