abfe024a2f5b93140dc8b4b484fa291d34ca96c0
Corri Golden <corri.golden@gmail.com>
2020-04-16 20:21:34 -0500

    installed fetch

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -2,75 +2,56 @@
     Define the keys and value for a JavaScript object that
     represents a journal entry about what you learned today
 */
-const journalEntries = [
-    {
-    date: "1/1/2020",
-    title: "JavaScript",
-    contents: "Functions",
-    mood: "sad"
-
-    },
-    {
-    date: "2/2/2020",
-    title: "React",
-    contents: "Capstone",
-    mood: "happy"
-    },
-    {
-    date: "3/3/2020",
-    title: "Django",
-    contents: "Full Stack",
-    mood: "Very Happy" 
-    }
-]
 
 
 
-const makeJournalEntryComponent = (title, date, contents) => {
+const makeJournalEntryComponent = (entry) => {
     return `
-        <h1>${title}</h1>
-        <h2>${date}</h2>
-        </h3>${contents}</h3>
+        <h1>${entry.title}</h1>
+        <h2>${entry.date}</h2>
+        </h3>${entry.contents}</h3>
     `
 }
 
 
+// Fetch from the API
+    fetch("http://localhost:3000/entries") 
+    .then(entries => entries.json())  // Parse as JSON
+    .then(parsedEntries => {
+        parsedEntries.forEach(entry => {
+        console.log(makeJournalEntryComponent(entry))
+        const journalContainer = document.querySelector(".entryLog")
+        journalContainer.innerHTML += makeJournalEntryComponent(entry)
+        // What should happen when we finally have the array?
+    })
+})
 
-    // for (const journalEntry of journalEntries){
-    //     const journalContainer = document.querySelector("#entrylog")
-    //     journalContainer.innerHTML = makeJournalEntryComponent()
-    // }
 
-    const renderJournalEntries = (entries) => {
-        for (const journalEntry of journalEntries){
+
+
+
+
+    
+
+    // const renderJournalEntries = (entries) => {
+    //     for (const journalEntry of journalEntries){
 
         
 
-        const journalContainer = document.querySelector(".entryLog")
-        journalContainer.innerHTML += makeJournalEntryComponent(
-            journalEntry.title,
-            journalEntry.date,
-            journalEntry.contents)
-        }
-    }
+    //     const journalContainer = document.querySelector(".entryLog")
+    //     journalContainer.innerHTML += makeJournalEntryComponent(
+    //         journalEntry.title,
+    //         journalEntry.date,
+    //         journalEntry.contents)
+    //     }
+    // }
 
-    console.log(journalEntries)
-    renderJournalEntries(journalEntries)
-
-
+    // console.log(journalEntries)
+    // renderJournalEntries(journalEntries)
 
 
-// const makeJournalEntryComponent = () => {
-//     return `
-//     <h1>Array Methods</h1>
-//     <h2>We learned about array methods, but only for Each made sense.</h2>
-//     <h2>07/24/2019</h2>
-//     `
-// }
 
 
-// const renderDom = document.querySelector(".entryLog")
-// renderDom.innerHTML = makeJournalEntryComponent()
 
 
    
